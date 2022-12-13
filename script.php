e4112d9240cfaff1b427d789184fbf7debb3af50
Никита Стригалев <nstrigalev@bk.ru>
2022-12-13 08:00:14 +0300

    first try

---
+++ script.php
@@ -0,0 +1,73 @@
+<?php header("Access-Control-Allow-Origin: *"); 
+    header("Access-Control-Allow-Methods: GET, POST"); 
+
+    if(!isset($_SESSION["status"])){
+        session_set_cookie_params(10000, "/");
+        session_start();
+    }
+    
+    function checkR($r){
+        return isset($r);
+    }
+
+    function checkX($x){
+        return isset($x);
+    }
+
+    function checkY($y){
+        if (!isset($y)){
+            return false;
+        }
+        $realY = str_replace(",", ".", $y);
+        return $realY <= 3 && $realY >= -3 && is_numeric($realY);
+    }
+
+    function validate($x, $y, $r){
+        return checkR($r) && checkX($x) && checkY($y);
+    }
+
+    function inSquare($x, $y, $r){
+        return ($x <= 0 && $y >= 0 && $x >= -$r && $y <= $r);
+    }
+
+    function inTriangle($x, $y, $r){
+        return ($x >= 0 && $y >= 0 && $y <= -2*$x + $r);
+    }
+
+    function inCircle($x, $y, $r){
+        return ($x <= 0 && $y <= 0 && sqrt($x*$x + $y*$y) <= $r/2);
+    }
+
+    function inArea($x, $y, $r){
+        return inSquare($x, $y, $r) || inTriangle($x, $y, $r) || inCircle($x, $y, $r);
+    }
+    date_default_timezone_set("Europe/Moscow");
+    $x = $_POST["xval"];
+    $y = $_POST["yval"];
+    $r = $_POST["rval"];
+    $valid = validate($x, $y, $r);
+    $isInArea = "false";
+    if (inArea($x, $y, $r)){
+        $isInArea = "true";
+    }
+    $executeTime = round(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'], 4);
+    $nowTime = date('H:i:s', time());
+    $answer = json_decode($_SESSION["status"]);
+    if ($valid){
+        $answer[] = array("x" => $x, "y" => $y, "r" => $r, "validate" => $valid, "nowTime" => $nowTime, "executeTime" => $executeTime, "inArea" => $isInArea);
+    }
+    $answer = json_encode($answer);
+    $_SESSION["status"] = $answer;
+    echo $_SESSION["status"];
+    //print_r($_SESSION["status"]);
+    //echo $answer;
+    //$answer = "x = {$x}, y = {$y}, r = {$r}, valid = {$valid}, inArea = {$isInArea}, executeTime = {$executeTime}, nowTime = {$nowTime}";
+    
+    /*$answer = "{" . "\"x\":\"" . $x . "\"," . 
+        "\"y\":\"" . $y . "\"," .
+        "\"r\":\"" . $r . "\"," . 
+        "\"validate\":\"" . $valid . "\"," . 
+        "\"nowTime\":\"" . $nowTime . "\"," . 
+        "\"executeTime\":\"" . $executeTime . "\"," . 
+        "\"inArea\":\"" . $isInArea . "\"}";*/
+    
---
+++ validate.js
@@ -0,0 +1,147 @@
+window.onload = function() {
+  alert('Страница загружена');
+
+  /**$.ajax({
+    url: 'http://localhost:3000/script.php',
+    method: 'POST',
+    data: $(this).serialize(),
+    dataType: "json",
+    beforeSend: function() {
+      $('.button').attr('disabled', 'disabled');
+    },
+    success: function(data) {
+      $('.button').attr('disabled', false);
+      //if (data.validate) {
+        alert(data);
+        data.array.forEach(({ x, y, r, validate, nowTime, executeTime, inArea}) => {
+          
+          //let y = data.y.replace(",", ".");
+          const table = document.getElementById("status-table");
+          const newRow = document.createElement("tr");
+          const x2 = document.createElement("td");
+          x2.innerText = x;
+          const y2 = document.createElement("td");
+          y2.innerText = y;
+          const r2 = document.createElement("td");
+          r2.innerText = r;
+          const executeTime2 = document.createElement("td");
+          executeTime2.innerText = executeTime;
+          const nowTime2 = document.createElement("td");
+          nowTime2.innerText = nowTime;
+          const inArea2 = document.createElement("td");
+          inArea2.innerText = inArea;
+          newRow.appendChild(x2);
+          newRow.appendChild(y2);
+          newRow.appendChild(r);
+          newRow.appendChild(executeTime);
+          newRow.appendChild(nowTime);
+          newRow.appendChild(inArea);
+          table.appendChild(newRow);
+        });
+        
+    
+        /*currentCase = '<tr>';
+        currentCase += '<td>' + data.x + '</td>';
+        currentCase += '<td>' + y + '</td>';
+        currentCase += '<td>' + data.r + '</td>';
+        currentCase += '<td>' + data.executeTime + '</td>';
+        currentCase += '<td>' + data.nowTime + '</td>';
+        currentCase += '<td>' + data.inArea + '</td>';
+        $('#status-table').append(currentCase);
+      //}
+    }
+  });*/
+  
+};
+$(function(){
+
+function checkR(){
+    if ($(".r-button").is(":checked")){
+        return true;
+    }
+    return false;
+}
+
+function checkX(){
+    if ($(".x-button").is(":checked")){
+        return true;
+      }
+    return false;
+}
+
+function checkY(){
+    let input = $("#text-y");
+    let y = input.val();
+    y = y.replace(",", ".");
+    if (isNumber(y) && y <= 3 && y >= -3){
+        return true;
+    }
+    return false;
+}
+
+function validate(){
+    return checkR() && checkX() && checkY();
+}
+
+function isNumber(a){
+    return isFinite(a) && !isNaN(parseFloat(a));
+}
+
+$('#dataform').on('submit', function(event) {
+    event.preventDefault();
+    if (!validate()){
+      alt = "Проверьте правильность следующих переменных: ";
+      if (!checkX()){
+        alt += "x, ";
+      }
+      if (!checkY()){
+        alt += "y, ";
+      }
+      if (!checkR()){
+        alt += "R, ";
+      }
+      alert(alt.substring(0, alt.length - 2));
+         return;
+    }
+    $.ajax({
+      url: 'http://localhost:3000/sanya.php',
+      method: 'POST',
+      data: $(this).serialize(),
+      dataType: "json",
+      beforeSend: function() {
+        $('.button').attr('disabled', 'disabled');
+      },
+      success: function(data) {
+        $('.button').attr('disabled', false);
+        data.forEach(({ x, y, r, validate, nowTime, executeTime, inArea }) => {
+          alert(x + y + r + validate + nowTime + executeTime + inArea + data.length);
+        });
+          /*
+          let y = data.y.replace(",", ".");
+          const table = document.getElementById("status-table");
+          const newRow = document.createElement("tr");
+          const x = document.createElement("td");
+          x.innerText = data.x;
+          const y2 = document.createElement("td");
+          y2.innerText = data.y;
+          const r = document.createElement("td");
+          r.innerText = data.r;
+          const executeTime = document.createElement("td");
+          executeTime.innerText = data.executeTime;
+          const nowTime = document.createElement("td");
+          nowTime.innerText = data.nowTime;
+          const inArea = document.createElement("td");
+          inArea.innerText = data.inArea;
+          newRow.appendChild(x);
+          newRow.appendChild(y2);
+          newRow.appendChild(r);
+          newRow.appendChild(executeTime);
+          newRow.appendChild(nowTime);
+          newRow.appendChild(inArea);
+          table.appendChild(newRow);
+      
+        }*/
+      }
+    });
+  });
+});
